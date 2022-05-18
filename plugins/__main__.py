79e6372f2156073a029bf7d09843c2e5a21ad5e6
FastUserBot <103589003+FastUserBot@users.noreply.github.com>
2022-05-18 11:33:46 +0400

    Update __main__.py

--- plugins/__main__.py
+++ plugins/__main__.py
@@ -1,7 +1,7 @@
 import os, heroku3, random, requests
 from time import time
 from git import Repo
-from brend_installer import *
+from plugin import *
 from .bstring import main
 from telethon import TelegramClient, functions
 from telethon.sessions import StringSession
