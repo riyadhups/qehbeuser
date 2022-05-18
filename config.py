b73a3e9b0bc89171c4b8b264e751392b1819542a
FastUserBot <103589003+FastUserBot@users.noreply.github.com>
2022-05-18 11:41:44 +0400

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-TG_BOT_TOKEN = "5350533280:AAFUxhuOC6Gavwk_eAsYpsPOOG3VZNfrkys" #botun tokeni
+TG_BOT_TOKEN = "5345700451:AAEB8iFQ7C4t6Cjjsv9_Zd_kUL2Klkbgces" #botun tokeni
 APP_ID = 17473863 #api id
 API_HASH = "976e92a7d32bbc0493e5f4f978a330ed" #api hash
