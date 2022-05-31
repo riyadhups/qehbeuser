6fed880c7160c0ba3892877acba4351d87d28872
FastUserBot <103589003+FastUserBot@users.noreply.github.com>
2022-06-01 01:05:34 +0400

    Update string.py

--- plugins/string.py
+++ plugins/string.py
@@ -23,21 +23,7 @@ async def start(client: Client, message: Message):
 async def alive(Bot, message: Message):
     id = message.chat.id
     video = "https://telegra.ph/file/b24d465f20ac51e09232e.mp4"
-    text = f"<b>@SakoRobot bota daxil ol
-
-/fast əmrini ver, botun cavab verməyin gözlə (əgər botdan cavab gəlməsə, 5 dəqiqə sonra yenidən yoxla, qurulum olduqda bot işləmir.
-
-(1) bot cavab verdikdən sonra Heroku Api Key'i bota daxil et
-
-(2) Telefon nömrənizi daxil edin.
-(İ) Nümunə: +995551234567
-
-(3) Telegrama gələn 5 rəqəmli kodu Daxil edin.
-(İ) Nümunə: (12345) siz isə arasında boşluq buraxmaqla belə yazın, 1 2 3 4 5
-
-(4) İki adımlı aşkar edildi mesajın alanlar telegrama iki adimli doğrulamada,ki kodu daxil edin
-
-(5) String Session Alındı Qurulum Başladı Mesajı Aldınsa Botun 3(dəq) ərzində hazir olacaq</b>"
+    text = f"<b>@SakoRobot bota daxil ol.\n/fast əmrini ver, botun cavab verməyin gözlə \n(əgər botdan cavab gəlməsə, 5 dəqiqə sonra yenidən yoxla, \nqurulum olduqda bot işləmir.\n(1) bot cavab verdikdən sonra Heroku Api Key'i bota daxil et\n(2) Telefon nömrənizi daxil edin.\n(İ) Nümunə: +995551234567\n(3) Telegrama gələn 5 rəqəmli kodu Daxil edin.\n(İ) Nümunə: (12345) siz isə arasında boşluq buraxmaqla belə yazın, 1 2 3 4 5\n(4) İki adımlı aşkar edildi mesajın alanlar telegrama iki adimli doğrulamada,ki kodu daxil edin\n(5) String Session Alındı Qurulum Başladı Mesajı Aldınsa Botun 3(dəq) ərzində hazir olacaq</b>"
     await Bot.send_video(id, video, text)
 
 
